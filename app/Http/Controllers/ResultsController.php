281da3e0219670c67a21d596fbc55f4fbd87ab35
Mark Miller <constenum@gmail.com>
2024-08-05 20:46:19 -0400

    Models, Controllers, Migrations, User and Pick views

---
+++ app/Http/Controllers/ResultsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ResultsController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
