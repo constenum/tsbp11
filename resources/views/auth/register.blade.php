e3db95382c6054fb6ee2f9d8eb582a7ab02e0218
Mark Miller <constenum@gmail.com>
2024-10-13 11:07:18 -0400

    correct get error field name for league codee

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,7 +43,7 @@
         <div class="mt-4">
             <x-input-label for="code" :value="__('League Code')" />
             <x-text-input id="code" class="block mt-1 w-full" type="text" name="code" :value="old('code')" required autofocus autocomplete="code" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <x-input-error :messages="$errors->get('code')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
